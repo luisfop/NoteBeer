8f96f352b8321cb7d352f26ff1b63f2fd66482a7
luisop <luisfop77@gmail.com>
2022-07-03 18:59:07 -0300

    feat: added what to do on the following week

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -8,6 +8,7 @@ const Form = () => {
     
     return (
         <Grid container>
+            {/* adicionar uma classe pro grid item e add tbm width: 100% */}
             <Grid item>
                 <Typography>Nome</Typography>
                 <TextField variant="outlined" className={classes.xablau} />
--- src/components/Form/FormStyle.ts
+++ src/components/Form/FormStyle.ts
@@ -3,6 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles((theme) => ({
   xablau: {
     color: theme.palette.primary.main,
+    width: '100%',
     
     "& .MuiOutlinedInput-root": {
       "& fieldset": {
